6325693c603755b90bb985257fd334a7a11df237
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-04-02 10:40:32 +0200

    using models and connect to the admin dashboard

--- os44/products/admin.py
+++ os44/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import Product
+
+admin.site.register(Product)
+
+## you can customize operations you need to appear in the admin dashboard
--- os44/products/models.py
+++ os44/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    ## define columns  ---> datatypes
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    code = models.CharField(max_length=100 , unique=True, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
