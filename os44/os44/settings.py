8b491a12b72da93b1623c0a664e65b88b01bdff1
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-04-02 10:10:54 +0200

    configure mysql-connection, run migrations, createsuperuser

--- os44/os44/settings.py
+++ os44/os44/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'os44.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'django_os44',
+        'USER': 'os44',
+        'PASSWORD': 'Iti123456789@#$',
+        'HOST': 'localhost',
+        'PORT':3306
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
